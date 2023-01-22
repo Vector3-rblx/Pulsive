8befac535448962b5af9393b5e2b5ec1c45ff6a7
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2023-01-21 20:22:38 -0500

    Update loaddata.lua

--- loaddata.lua
+++ loaddata.lua
@@ -1,4 +1,4 @@
-shared.loaddata = {236309920, 459208566, 4236112792}
+shared.loaddata = {236309920, 459208566, 4236112792, 4236309920}
 shared.Funny = {4236112792}
  pcall(function()
 for i,v in pairs(shared.Funny) do
