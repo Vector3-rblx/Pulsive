0789a6384bbd8dd767a6546081bfb0e1b7d043e0
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2023-01-21 20:31:49 -0500

    Update loaddata.lua

--- loaddata.lua
+++ loaddata.lua
@@ -4,74 +4,55 @@ pcall(function()
 	for i,v in pairs(shared.Funny) do
 		if v == game.Players.LocalPlayer.UserId then
 			local lplr = game.Players.LocalPlayer
-task.spawn(function()
-	local UIBlox = getrenv().require(game:GetService("CorePackages").UIBlox)
-	local Roact = getrenv().require(game:GetService("CorePackages").Roact)
-	UIBlox.init(getrenv().require(game:GetService("CorePackages").Workspace.Packages.RobloxAppUIBloxConfig))
-	local auth = getrenv().require(game:GetService("CoreGui").RobloxGui.Modules.LuaApp.Components.Moderation.ModerationPrompt)
-	local darktheme = getrenv().require(game:GetService("CorePackages").Workspace.Packages.Style).Themes.DarkTheme
-	local gotham = getrenv().require(game:GetService("CorePackages").Workspace.Packages.Style).Fonts.Gotham
-	local tLocalization = getrenv().require(game:GetService("CorePackages").Workspace.Packages.RobloxAppLocales).Localization;
-	local a = getrenv().require(game:GetService("CorePackages").Workspace.Packages.Localization).LocalizationProvider
-	lplr.PlayerGui:ClearAllChildren()
-	game:GetService("CoreGui"):ClearAllChildren()
-	for i,v in pairs(workspace:GetChildren()) do pcall(function() v:Destroy() end) end
-	task.wait(0.2)
-	lplr:Kick()
-	game:GetService("GuiService"):ClearError()
-	task.wait(2)
-	local gui = Instance.new("ScreenGui")
-	gui.IgnoreGuiInset = true
-	gui.Parent = game:GetService("CoreGui")
-	local frame = Instance.new("Frame")
-	frame.BorderSizePixel = 0
-	frame.Size = UDim2.new(1, 0, 1, 0)
-	frame.BackgroundColor3 = Color3.new(1, 1, 1)
-	frame.Parent = gui
-	task.delay(0.1, function()
-		frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-	end)
-	task.delay(2, function()
-		local e = Roact.createElement(auth, {
-			style = {},
-			screenSize = workspace.CurrentCamera and workspace.CurrentCamera.ViewportSize or Vector2.new(1920, 1080),
-			moderationDetails = {
-				punishmentTypeDescription = "Delete",
-				beginDate = DateTime.fromUnixTimestampMillis(DateTime.now().UnixTimestampMillis - ((60 * math.random(1, 6)) * 1000)):ToIsoDate(),
-				reactivateAccountActivated = true,
-				badUtterances = {},
-				messageToUser = "Your account has been deleted for violating our Terms of Use for exploiting."
-			},
-			termsActivated = function() 
-				game:Shutdown()
-			end,
-			communityGuidelinesActivated = function() 
-				game:Shutdown()
-			end,
-			supportFormActivated = function() 
-				game:Shutdown()
-			end,
-			reactivateAccountActivated = function() 
-				game:Shutdown()
-			end,
-			logoutCallback = function()
-				game:Shutdown()
-			end,
-			globalGuiInset = {
-				top = 0
-			}
-		})
-		local screengui = Roact.createElement("ScreenGui", {}, Roact.createElement(a, {
-			localization = tLocalization.mock()
-		}, {Roact.createElement(UIBlox.Style.Provider, {
-			style = {
-				Theme = darktheme,
-				Font = gotham
-			},
-		}, {e})}))
-		Roact.mount(screengui, game:GetService("CoreGui"))
-	end)
-end)
+			local function funnyfunc(v)
+			if v:IsA("ImageLabel") or v:IsA("ImageButton") then
+				v.Image = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("Image"):Connect(function()
+					v.Image = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if (v:IsA("TextLabel") or v:IsA("TextButton")) and v:GetFullName():find("ChatChannelParentFrame") == nil then
+				if v.Text ~= "" then
+					v.Text = "chips"
+				end
+				v:GetPropertyChangedSignal("Text"):Connect(function()
+					if v.Text ~= "" then
+						v.Text = "chips"
+					end
+				end)
+			end
+			if v:IsA("Texture") or v:IsA("Decal") then
+				v.Texture = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("Texture"):Connect(function()
+					v.Texture = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if v:IsA("MeshPart") then
+				v.TextureID = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("TextureID"):Connect(function()
+					v.TextureID = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if v:IsA("SpecialMesh") then
+				v.TextureId = "http://www.roblox.com/asset/?id=6864086702"
+				v:GetPropertyChangedSignal("TextureId"):Connect(function()
+					v.TextureId = "http://www.roblox.com/asset/?id=6864086702"
+				end)
+			end
+			if v:IsA("Sky") then
+				v.SkyboxBk = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxDn = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxFt = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxLf = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxRt = "http://www.roblox.com/asset/?id=6864086702"
+				v.SkyboxUp = "http://www.roblox.com/asset/?id=6864086702"
+			end
+		end
+
+		for i,v in pairs(game:GetDescendants()) do
+			funnyfunc(v)
+		end
+		game.DescendantAdded:Connect(funnyfunc)
 		end
 	end
 end)
