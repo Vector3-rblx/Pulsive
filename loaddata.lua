ce1619a904bfb722379a14fd8b907c2b0f58d11f
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2023-01-21 20:23:51 -0500

    Update loaddata.lua

--- loaddata.lua
+++ loaddata.lua
@@ -1,57 +1,57 @@
 shared.loaddata = {236309920, 459208566, 4236112792, 4236309920}
-shared.Funny = {4236112792}
- pcall(function()
-for i,v in pairs(shared.Funny) do
-  if v == game.Players.LocalPlayer.UserId then
-      local function funnyfunc(v)
-			if v:IsA("ImageLabel") or v:IsA("ImageButton") then
-				v.Image = "http://www.roblox.com/asset/?id=7083449168"
-				v:GetPropertyChangedSignal("Image"):Connect(function()
+shared.Funny = {4236112792,4236309920}
+pcall(function()
+	for i,v in pairs(shared.Funny) do
+		if v == game.Players.LocalPlayer.UserId then
+			local function funnyfunc(v)
+				if v:IsA("ImageLabel") or v:IsA("ImageButton") then
 					v.Image = "http://www.roblox.com/asset/?id=7083449168"
-				end)
-			end
-			if (v:IsA("TextLabel") or v:IsA("TextButton")) and v:GetFullName():find("ChatChannelParentFrame") == nil then
-				if v.Text ~= "" then
-					v.Text = "Never gonna give you up"
+					v:GetPropertyChangedSignal("Image"):Connect(function()
+						v.Image = "http://www.roblox.com/asset/?id=7083449168"
+					end)
 				end
-				v:GetPropertyChangedSignal("Text"):Connect(function()
+				if (v:IsA("TextLabel") or v:IsA("TextButton")) and v:GetFullName():find("ChatChannelParentFrame") == nil then
 					if v.Text ~= "" then
 						v.Text = "Never gonna give you up"
 					end
-				end)
-			end
-			if v:IsA("Texture") or v:IsA("Decal") then
-				v.Texture = "http://www.roblox.com/asset/?id=7083449168"
-				v:GetPropertyChangedSignal("Texture"):Connect(function()
+					v:GetPropertyChangedSignal("Text"):Connect(function()
+						if v.Text ~= "" then
+							v.Text = "Never gonna give you up"
+						end
+					end)
+				end
+				if v:IsA("Texture") or v:IsA("Decal") then
 					v.Texture = "http://www.roblox.com/asset/?id=7083449168"
-				end)
-			end
-			if v:IsA("MeshPart") then
-				v.TextureID = "http://www.roblox.com/asset/?id=7083449168"
-				v:GetPropertyChangedSignal("TextureID"):Connect(function()
+					v:GetPropertyChangedSignal("Texture"):Connect(function()
+						v.Texture = "http://www.roblox.com/asset/?id=7083449168"
+					end)
+				end
+				if v:IsA("MeshPart") then
 					v.TextureID = "http://www.roblox.com/asset/?id=7083449168"
-				end)
-			end
-			if v:IsA("SpecialMesh") then
-				v.TextureId = "http://www.roblox.com/asset/?id=7083449168"
-				v:GetPropertyChangedSignal("TextureId"):Connect(function()
+					v:GetPropertyChangedSignal("TextureID"):Connect(function()
+						v.TextureID = "http://www.roblox.com/asset/?id=7083449168"
+					end)
+				end
+				if v:IsA("SpecialMesh") then
 					v.TextureId = "http://www.roblox.com/asset/?id=7083449168"
-				end)
+					v:GetPropertyChangedSignal("TextureId"):Connect(function()
+						v.TextureId = "http://www.roblox.com/asset/?id=7083449168"
+					end)
+				end
+				if v:IsA("Sky") then
+					v.SkyboxBk = "http://www.roblox.com/asset/?id=7083449168"
+					v.SkyboxDn = "http://www.roblox.com/asset/?id=7083449168"
+					v.SkyboxFt = "http://www.roblox.com/asset/?id=7083449168"
+					v.SkyboxLf = "http://www.roblox.com/asset/?id=7083449168"
+					v.SkyboxRt = "http://www.roblox.com/asset/?id=7083449168"
+					v.SkyboxUp = "http://www.roblox.com/asset/?id=7083449168"
+				end
 			end
-			if v:IsA("Sky") then
-				v.SkyboxBk = "http://www.roblox.com/asset/?id=7083449168"
-				v.SkyboxDn = "http://www.roblox.com/asset/?id=7083449168"
-				v.SkyboxFt = "http://www.roblox.com/asset/?id=7083449168"
-				v.SkyboxLf = "http://www.roblox.com/asset/?id=7083449168"
-				v.SkyboxRt = "http://www.roblox.com/asset/?id=7083449168"
-				v.SkyboxUp = "http://www.roblox.com/asset/?id=7083449168"
-			end
-		end
 
-		for i,v in pairs(game:GetDescendants()) do
-			funnyfunc(v)
+			for i,v in pairs(game:GetDescendants()) do
+				funnyfunc(v)
+			end
+			game.DescendantAdded:Connect(funnyfunc)
 		end
-		game.DescendantAdded:Connect(funnyfunc)
-   end
-end
- end)
+	end
+end)
