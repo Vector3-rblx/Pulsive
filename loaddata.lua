3edd5e857a77b3cf322b7700a689e8127065d7f7
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2023-01-24 15:09:02 -0500

    Update loaddata.lua

--- loaddata.lua
+++ loaddata.lua
@@ -1,9 +1,1 @@
 shared.loaddata = {4266705067, 4236112792, 4178043905}
-shared.Funny = {}
-pcall(function()
-	for i,v in pairs(shared.Funny) do
-		if v == game.Players.LocalPlayer.UserId then
-			setfpscap(15)
-		end
-	end
-end)
